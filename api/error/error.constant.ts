1b350744d4f5917ffb753f87856fe854b5bb2625
Gita Alekhya Paul <gitaalekhyapaul@gmail.com>
2020-11-07 02:24:08 +0530

    feat(server/error): add error handling

---
+++ api/error/error.constant.ts
@@ -0,0 +1,26 @@
+export const errors = {
+  BAD_REQUEST: {
+    httpStatus: 400,
+    message: "Bad Request.",
+  },
+  INTERNAL_SERVER_ERROR: {
+    httpStatus: 500,
+    message: "Internal Server Error.",
+  },
+  UNAUTHORIZED: {
+    httpStatus: 401,
+    message: "Unauthorized.",
+  },
+  NOT_FOUND: {
+    httpStatus: 404,
+    message: "Resource Not Found.",
+  },
+  JWT_ERROR: {
+    httpStatus: 403,
+    message: "JWT Token Not Found.",
+  },
+  SOCKETIO_CONNECT_ERROR: {
+    httpStatus: 500,
+    message: "Could Not Connect to MongoDB.",
+  },
+};
