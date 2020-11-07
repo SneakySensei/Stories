7eddc2d50d945e8a902d684bf8b504d15a695e40
Yashvardhan Jagnani <jagnani73@users.noreply.github.com>
2020-11-07 20:23:07 +0530

    feat(client): gauth init

--- client/src/components/Index/Hero.tsx
+++ client/src/components/Index/Hero.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { createBrowserHistory } from "history";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+import { getGAuth } from "../../services/axios";
 import { ReactComponent as LandingImg } from "../../assets/landingRight.svg";
 import { ReactComponent as TopImg } from "../../assets/top.svg";
 import googleNormal from "../../assets/google/google_normal.svg";
@@ -110,6 +112,23 @@ const GoogleButton = styled.a`
 `;
 
 const Hero = () => {
+  const [isAuth, setIsAuth] = useState<boolean>(false);
+
+  useEffect(() => {
+    const tokenx64 = window.location.search.substring(
+      7,
+      window.location.search.length
+    );
+    const token = atob(tokenx64);
+    sessionStorage.setItem("token", token);
+    let history = createBrowserHistory();
+    history.replace("/");
+  });
+
+  const GAuthHandler = () => {
+    getGAuth();
+  };
+
   return (
     <Landing>
       <div className="left-section">
@@ -121,9 +140,11 @@ const Hero = () => {
           <div>I'm feeling helpful</div>
         </Link>
 
-        <GoogleButton>
+        <GoogleButton onClick={GAuthHandler}>
           <span className="icon"></span>
-          <span className="label">Sign in with Google</span>
+          <span className="label">
+            {isAuth ? "Signed in" : "Sign in with Google"}
+          </span>
         </GoogleButton>
       </div>
       <div className="right-section">
